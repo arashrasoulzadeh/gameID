e774abacf8a86ce5e986c463b70cecb8eacf6e69
Arash Rasoulzadeh <arashrasoulzadeh@gmail.com>
2015-07-09 03:47:20 +0430

    Laravel 5 loaded!

--- modules/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-function getRow($sql)
-{
-  $result = mysql_query($sql);
-  $row = mysql_fetch_row($result);
-  return $row;
-} ?>
---
+++ resources/views/apifile.php
@@ -0,0 +1,1 @@
+if you have any questions , contact us at arashrasoulzadeh@gmail.com
